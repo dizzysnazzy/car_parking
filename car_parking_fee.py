1106018e2673f84cc529a75c886a484e3fb73404
dizzysnazzy <dismaskipchumba2@gmail.com>
2023-02-11 19:41:23 +0300

    Scan number plate changes

--- car_parking_fee.py
+++ car_parking_fee.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 class CarGate:
 
 
@@ -8,7 +9,10 @@ class CarGate:
         self.payment_status = {}
 
 
-    def scan_number_plate(self, number_plate):
+    def scan_number_plate(self):
+        # Scan the car's number plate
+        number_plate = input("Scanning car number plate...")
+
         # Call the car number plate API to get owner's details
         response = request.get(f"https://api.example.com/number_plate/{number_plate}")
         owner_details = json.loads(response.text)
@@ -32,15 +36,15 @@ class CarGate:
             print("Gate opened. Enjoy your drive!")
         else:
             print("Payment required. Please make payment and try again.")
+
 #Initialize the CarGate object
 car_gate = CarGate()
 
-# Simulate a car entering the gate
-number_plate = input("Enter car number plate:")
-owner_details = car_gate.scan_number_plate(number_plate)
+# Scan the car's number plate
+owner_details = car_gate.scan_number_plate()
 
-if number_plate not in car_gate.payment_status:
+if owner_details['number_plate'] not in car_gate.payment_status:
     # Car has not made payment before
     car_gate.prompt_mpesa_payment(owner_details, 100)
 
-car_gate.open_gate(number_plate)
+car_gate.open_gate(owner_details['number_plate'])
