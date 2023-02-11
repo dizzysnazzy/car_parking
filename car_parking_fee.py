0c79898c40d8ab6ba8c7ec543b5c2bcbec8c528b
dizzysnazzy <dismaskipchumba2@gmail.com>
2023-02-11 14:30:36 +0300

    prompt for MPesa payment

--- car_parking_fee.py
+++ car_parking_fee.py
@@ -14,3 +14,13 @@ class CarGate:
         owner_details = json.loads(response.text)
 
         return owner_details
+
+
+    def prompt_mpesa_payment(self, owner_details, amount):
+        # Prompt for MPesa payment
+        print(f"Payment of KES {amount} requested for car with number plate {owner_details['number_plate']}")
+        print(f"Owned by {owner_details['owner_name']} ({owner_details['owner_phone']})")
+
+        # Assuming payment is successful
+        self.payment_status[owner_details['number_plate']] = True
+        print("Payment successful!")
