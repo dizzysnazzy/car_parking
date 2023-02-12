af488bc141308fcdd3f9da8cb79bc5508ebb06fa
dizzysnazzy <dismaskipchumba2@gmail.com>
2023-02-12 11:30:41 +0300

    syntax correction

--- car_parking_fee.py
+++ car_parking_fee.py
@@ -14,7 +14,7 @@ class CarGate:
         number_plate = input("Scanning car number plate...")
 
         # Call the car number plate API to get owner's details
-        response = request.get(f"https://api.example.com/number_plate/{number_plate}")
+        response = requests.get(f"https://api.example.com/number_plate/{number_plate}")
         owner_details = json.loads(response.text)
 
         return owner_details
